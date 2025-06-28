7ba49bca04a6b6d54d104fd6816b97cdfe75c498
Datdater <datnqse173081@fpt.edu.vn>
2025-06-28 22:43:22 +0700

    Add migration changeaddressv2 for Stores table update
    This migration modifies the `Stores` table by dropping the `UserId` column and making the `AppUserId` column non-nullable. It also establishes a foreign key relationship between `AppUserId` in the `Stores` table and the `Id` column in the `AspNetUsers` table with cascade delete behavior. The `Down` method restores the previous state by re-adding the `UserId` columns and making `AppUserId` nullable again.

---
+++ FurEverCarePlatform.Persistence/Migrations/20250628154211_change-address-v2.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FurEverCarePlatform.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeaddressv2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Stores_AspNetUsers_AppUserId",
+                table: "Stores");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Stores");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Addresses");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AppUserId",
+                table: "Stores",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Stores_AspNetUsers_AppUserId",
+                table: "Stores",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Stores_AspNetUsers_AppUserId",
+                table: "Stores");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AppUserId",
+                table: "Stores",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Stores",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Addresses",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Stores_AspNetUsers_AppUserId",
+                table: "Stores",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}

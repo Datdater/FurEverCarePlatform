ce1c5cbdb28952870cdda4fb5f20ddb503ff9be2
doanhung <hungdmse173190@fpt.edu.vn>
2025-06-29 15:41:20 +0700

    Add migrations to update Pets table structure
    This commit introduces two migrations: `FixPet` and `FixWeight`.
    The `FixPet` migration removes the `UserId` column from the `Pets` table, while the `FixWeight` migration adds a new `Weight` column.
    Designer files for both migrations have been updated to reflect the changes in the model structure and entity relationships.

---
+++ FurEverCarePlatform.Persistence/Migrations/20250629082549_FixPet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FurEverCarePlatform.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Pets");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Pets",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}

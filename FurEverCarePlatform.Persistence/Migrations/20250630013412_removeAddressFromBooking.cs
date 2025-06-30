d2979d79c5781a1f6c775025e83dd79096ea0957
doanhung <hungdmse173190@fpt.edu.vn>
2025-06-30 09:01:51 +0700

    Remove AddressId from Bookings and add Deliveries table
    Created migration `removeAddressFromBooking` to remove the `AddressId` column from the `Bookings` table. The `Down` method restores the `AddressId` and adds a new `DeliveryId` column. A new `Deliveries` table is created with fields for delivery details, and foreign keys are established to link `Bookings` to both `Addresses` and `Deliveries`.

---
+++ FurEverCarePlatform.Persistence/Migrations/20250630013412_removeAddressFromBooking.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FurEverCarePlatform.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeAddressFromBooking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "AddressId",
+                table: "Bookings",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "DeliveryId",
+                table: "Bookings",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Deliveries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: true),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DeleteDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    DeletedBy = table.Column<Guid>(type: "uuid", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    Max = table.Column<float>(type: "real", nullable: false),
+                    Min = table.Column<float>(type: "real", nullable: false),
+                    ModificationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    ModifiedBy = table.Column<Guid>(type: "uuid", nullable: true),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deliveries", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_AddressId",
+                table: "Bookings",
+                column: "AddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_DeliveryId",
+                table: "Bookings",
+                column: "DeliveryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Addresses_AddressId",
+                table: "Bookings",
+                column: "AddressId",
+                principalTable: "Addresses",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Deliveries_DeliveryId",
+                table: "Bookings",
+                column: "DeliveryId",
+                principalTable: "Deliveries",
+                principalColumn: "Id");
+        }
+    }
+}

708d4aa7d49391031a69c55a4ead2e3819bd626d
Datdater <datnqse173081@fpt.edu.vn>
2025-07-09 16:35:33 +0700

    Remove UserId from UpdateCartItemRequest class
    Eliminated the UserId property and its assignment in the constructor, simplifying the class. This change suggests that user context is no longer necessary for updating cart items.

--- FurEverCarePlatform.Application/Models/UpdateCartItemRequest.cs
+++ FurEverCarePlatform.Application/Models/UpdateCartItemRequest.cs
@@ -2,13 +2,11 @@
 {
     public class UpdateCartItemRequest
     {
-        public string? UserId { get; set; }
         public string Id { get; set; } // The ID of the cart item to update
         public int Quantity { get; set; } // The new quantity for the cart item
 
         public UpdateCartItemRequest(string userId, string id, int quantity)
         {
-            UserId = userId;
             Id = id;
             Quantity = quantity;
         }
